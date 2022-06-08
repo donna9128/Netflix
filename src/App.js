de8d737737ee315db1f88aeab7f6c8c26c2eb418
Rich <rich@richs-mbp.mynetworksettings.com>
2022-06-08 16:24:04 -0400

    the api sends images to react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movies: {
+        results: []
+      }
+    }
+  }
+
+  componentDidMount() {
+    // this is where you load things from the internet
+    fetch('https://api.themoviedb.org/3/discover/movie?api_key=b6fbc7f3f313bd395902af464ef47262&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate')
+      .then(response => response.json())
+      .then(data => this.setState({movies: data}));
+  }
+
+  render() {
+    const movies = [];
+    for(let i = 0; i < this.state.movies.results.length; i++)
+    {
+      movies.push(<div className="itemContainer">
+        <li className="item">
+          <img className="image" src={"https://image.tmdb.org/t/p/w200/" + this.state.movies.results[i].poster_path
+}/></li>
+      
+      </div>)
+    }
+
+    // const movies = this.state.movieTitles.map(movie => {
+    //   return <li>{movie}</li>
+    // });
+
+    return (
+      <div className="App">
+        <ul className="list">
+          {movies}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
